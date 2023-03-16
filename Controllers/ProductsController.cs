e02fa0407fae9f58390edbef0679baa133448ee8
konika-jindal <Konikajindal1783@gmail.com>
2023-03-16 14:58:26 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using products_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace products_mvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: ProductsController
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+        public ActionResult Index()
+        {
+            return View( _context..ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,Name,Price,Color,ProductCategory")] Product pd)
+        {
+
+            if (ModelState.IsValid)
+            { 
+                _context.Add(pd);
+            _context.SaveChangesAsync();
+            return RedirectToAction("Index"); 
+            }
+            return View(pd);
+
+            
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context..FirstOrDefaultAsync(m => m.Id == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+
+    }
+}
---
+++ Models/ProductContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace products_mvc.Models
+{
+    public partial class ProductContext : DbContext
+    {
+        public ProductContext()
+        {
+        }
+
+        public ProductContext(DbContextOptions<ProductContext> options): base(options)
+        {
+        }
+
+        public virtual DbSet<Product>  { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Color)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductCategory).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        
+    }
+}
